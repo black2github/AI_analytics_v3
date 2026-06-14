a708ea1def7afd25d18f8e88ac1e6f521a57d99f
black <alexey.sen@gmail.com>
2026-06-15 01:20:14 +0300

    атрибут status из frontmatter проставляется на основе сравнения подтвержденных и неподтвержденных требований. Правда новые страницы автоматом будут считаться подтвержденными.

--- app/config.py
+++ app/config.py
@@ -13,7 +13,7 @@ except Exception:
 
 load_dotenv()
 
-APP_VERSION = os.getenv("APP_VERSION", "0.90.1")
+APP_VERSION = os.getenv("APP_VERSION", "0.91.0")
 
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 ANTHROPIC_API_KEY = os.getenv("ANTHROPIC_API_KEY")
